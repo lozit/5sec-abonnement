0f9ce2c332d962187535a492a2bcd6f3e8a097ae
Guillaume Ferrari <guillaume.ferrari@gmail.com>
2020-10-14 15:06:57 +0200

    nettoyage + personnalisation

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -6,9 +6,9 @@ const content = {
     credit: "Jacob Van Blarcom",
     creditLink: "https://unsplash.com/photos/lkzjENdWgd8",
     message: "To view this content, you need to create an account!",
-    allowedRoles: ["free", "pro", "premium"],
+    allowedRoles: ["free", "small", "medium", "large"],
   },
-  pro: {
+  abonne: {
     src:
       "https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80",
     alt: "close-up of a corgi with its tongue hanging out",
@@ -16,17 +16,7 @@ const content = {
     creditLink: "https://unsplash.com/photos/yxmNWxi3wCo",
     message:
       "This is protected content! It’s only available if you have a pro plan or higher.",
-    allowedRoles: ["pro", "premium"],
-  },
-  premium: {
-    src:
-      "https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80",
-    alt: "corgi in a tent with string lights in the foreground",
-    credit: "Cole Keister",
-    creditLink: "https://unsplash.com/photos/cX-KEISwDIw",
-    message:
-      "This is protected content! It’s only available if you have the premium plan.",
-    allowedRoles: ["premium"],
+    allowedRoles: ["small", "medium", "large"],
   },
 };
 
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,4 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Demo of the Demo',
-
-  // these are all optional and add links to the footer
-  repo: 'learnwithjason/demo-base',
-  episode:
-    'https://www.learnwithjason.dev/creating-css-variable-font-text-effects',
-  tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
+  title: "Cinq Secondes",
 };
